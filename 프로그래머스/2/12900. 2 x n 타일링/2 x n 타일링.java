6247dc513269fbe41ca2b7cc57d0505a4678e1e9
Doyeon Lee (Luna) <ldon77@naver.com>
2025-02-24 09:48:50 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.98 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int[] dp = new int[60001];
+    
+    public int solution(int n) {
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for ( int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+        
+        return dp[n];
+    }
+}
