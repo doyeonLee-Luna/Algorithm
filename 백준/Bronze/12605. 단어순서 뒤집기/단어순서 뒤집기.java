9ee99ce2d0fad2629ea0d97711987e0253d99165
Doyeon Lee (Luna) <ldon77@naver.com>
2025-02-25 23:38:58 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 124 ms, Memory: 14744 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			
+			String[] words = line.split(" ");
+			
+			Stack<String> stack = new Stack<>();
+			
+			for(String word : words) {
+				stack.push(word);
+			}
+			
+			StringBuilder sb = new StringBuilder();
+			while( !stack.isEmpty()) {
+				sb.append(stack.pop());
+				if (!stack.isEmpty()) {
+					sb.append(" "); // 공백을 추가해주기
+				}
+				
+				}
+			
+			System.out.println("Case #" + (i+1) + ": " + sb.toString());
+		}
+		
+			
+		}
+
+	}
