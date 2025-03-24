84ae05a90f662e509ec7c150cbdafb1a58e2a770
Doyeon Lee (Luna) <ldon77@naver.com>
2025-03-24 21:17:03 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,20 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int A = Integer.parseInt(br.readLine());
+	
+		System.out.println(A-543);
+		
+
+	}
+
+}
