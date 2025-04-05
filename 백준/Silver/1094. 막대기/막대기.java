fd3396c80b1c0bfd01f0bbaa1775bd9277de582d
Doyeon Lee (Luna) <ldon77@naver.com>
2025-04-05 22:03:11 +0900

    [Silver V] Title: 막대기, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,38 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+	
+	
+		int stick = 64;
+		int count = 0;
+		
+		if ( X == 64 ) {
+			System.out.println(1);
+			return;
+		}
+	
+		while ( stick != 1 ) {
+			
+				stick = stick >> 1;
+				
+				if ( stick <= X) {
+					X = X - stick;
+					count++;
+				
+				}
+			}
+			
+		System.out.println(count);
+		}
+		
+		
+	}
+
+
