5a4c6d071e83123335ff176841c6f66825ff18c9
Doyeon Lee (Luna) <ldon77@naver.com>
2025-06-01 23:32:36 +0900

    [level 2] Title: 기능개발, Time: 18.16 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,31 +1,18 @@
-import java.util.*;
 
+// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
-        Queue<Integer> answer = new ArrayDeque<>();
-        
-        int n = progresses.length;
-        
-        int[] daysLeft = new int[n];
-        for ( int i = 0; i < n; i++ ) {
-            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-        }
-        int count = 0;
-        int maxDay = daysLeft[0];
-        
-        for ( int i = 0 ; i < n; i++ ) {
-            if ( daysLeft[i] <= maxDay) {
-                count++;
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
             }
-            else {
-                answer.add(count);
-                count = 1;
-                maxDay = daysLeft[i];
-            }
+            dayOfend[day]++;
         }
-        
-        answer.add(count);
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
