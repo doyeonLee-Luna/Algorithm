a4d4ce61dd63c4e79893ee58da89572b748f14a0
Doyeon Lee (Luna) <ldon77@naver.com>
2025-06-24 00:18:07 +0900

    [level 0] Title: 문자열 밀기, Time: 12.86 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,24 +1,6 @@
-public class Solution {
-    public static int solution(String A, String B) {
-        if (A.length() != B.length()) {
-            return -1; // 길이가 다르면 절대 될 수 없음
-        }
-
-        for (int i = 0; i < A.length(); i++) {
-            if (A.equals(B)) {
-                return i;
-            }
-            A = A.charAt(A.length() - 1) + A.substring(0, A.length() - 1);
-        }
-
-        return -1;
-    }
-
-    // 테스트용 main 메소드
-    public static void main(String[] args) {
-        System.out.println(solution("hello", "ohell")); // 1
-        System.out.println(solution("apple", "elppa")); // -1
-        System.out.println(solution("atat", "tata"));   // 1
-        System.out.println(solution("abc", "abc"));     // 0
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        return answer;
     }
 }
