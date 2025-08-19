4819d6ce371409b7b6259832365829222583b865
Doyeon Lee (Luna) <ldon77@naver.com>
2025-08-19 22:31:57 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int number = Integer.parseInt(br.readLine());
+
+        if ( number % 4 == 0 && number % 100 != 0 || number % 400 == 0) {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+
+    }
+
+
+}
