ca308651bb61ef545432eded2a5501f04aef5c49
Doyeon Lee (Luna) <ldon77@naver.com>
2025-08-20 21:25:11 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,35 @@
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        String n = br.readLine();
+        char c = n.charAt(0);
+        int ascii = (int) c;
+
+        System.out.println(ascii);
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
