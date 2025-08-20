1f7d9b14c1f51ed25c76e84d4cea473557a71cb1
Doyeon Lee (Luna) <ldon77@naver.com>
2025-08-20 21:37:13 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+		        System.out.print("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+	}
+
+
+
+
+
+
+
+
+    
+
+
