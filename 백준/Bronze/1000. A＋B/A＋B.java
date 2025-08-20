ec8edb947228f996a21ce9f7c2b1b7d3fbed3426
Doyeon Lee (Luna) <ldon77@naver.com>
2025-08-20 13:36:55 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,21 +1,29 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		
-		
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
 
-		System.out.println(A+B);
 
-	}
+
+        
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int n = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+
+                int sum = n + x;
+                System.out.println(sum);
+            
+
+
+
+
+    }
+
 
 }
