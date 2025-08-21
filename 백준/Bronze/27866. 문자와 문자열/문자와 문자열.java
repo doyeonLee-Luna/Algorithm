983246b6eed37e1f2a044f1900d01135938c0b95
Doyeon Lee (Luna) <ldon77@naver.com>
2025-08-21 14:05:42 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        String n = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+        
+   
+        char c = n.charAt(N-1);
+
+        System.out.println(c);
+        
+
+
+      
+
+
+
+        }
+
+
+
+
+
+
+
+    }
